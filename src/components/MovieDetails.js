b4f66be7bd602a01368fc65d51e04bfeb8c35bd3
Gabrielbreeding <36171277+Gabrielbreeding@users.noreply.github.com>
2021-11-09 14:17:47 -0600

    initial commit
    initial commit including:
        working version,
        "good" looking CSS.

---
+++ src/components/MovieDetails.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+
+function MovieDetails(properties) {
+    const [movieDetails, setMovieDetails] = useState({});
+
+    useEffect(() => {
+        const imdbID = properties.match.params.imdbID;
+        console.log(properties.match.params.imdbID);
+
+        extractMovieDetailsByID(imdbID);
+    }, []);
+
+    const extractMovieDetailsByID = (imdbID) => {
+        const movieDetailsURL = `https://www.omdbapi.com/?i=${imdbID}&apikey=b397790b`;
+        fetch(movieDetailsURL)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+            setMovieDetails(result)
+        });
+    };
+    return (
+        <div className="justify-content-center">
+            <div className="container">
+                <div id="header" className="col mb-4">
+                    <div className="row justify-content-center mt-3">
+                        <h1>Details on {movieDetails.Title}</h1>
+                    </div>
+                </div>
+                
+                <div className="row justify-content-center">
+                    <div className="col container">
+                        <img className="ml-5" width="400px" height="600px" src={movieDetails.Poster} alt={"Poster for "+movieDetails.Title}/>
+                    </div>
+                    <div className="col">
+                        <h6>{movieDetails.Plot}</h6>
+                    </div>
+                </div>
+            </div>
+
+            <div id="footer" className="row justify-content-center pt-4 pb-3 mt-2">
+                    <a className="mr-4" href="#search-bar">Search</a>
+                    <p>|</p>
+                    <a className="ml-4" target="_blank" href="http://iwt.ranken.edu/gbreeding/BlogSite/">Developer</a>
+            </div>
+        </div>
+    );
+};
+
+export default MovieDetails;
---
+++ src/components/MovieList.js
@@ -0,0 +1,87 @@
+// get imports
+import React, { useState, useEffect } from 'react';
+import {NavLink} from 'react-router-dom';
+
+function MovieList () {
+    // set up constant variables
+    const [searchTerm, setSearchTerm] = useState(''); // creates a state value that holds a string
+    const [movies, setMovies] = useState([]);         // creates a state value that holds an array
+    const [noMovieFound, setNoMovieFound] = useState(false);
+
+    const handleChange = (event) => { // event = onChange, target = object calling the function, value = 
+        setSearchTerm(event.target.value);
+    };
+
+    const handleClearScreen = () => {
+        setMovies([]);
+        setSearchTerm("");
+        localStorage.removeItem("searchTerm");
+        setNoMovieFound("");
+    };
+
+    const handleClick = (movie) => {
+        const movieURL = `http://www.omdbapi.com/?s=${movie}&apikey=b397790b`;
+
+        localStorage.setItem("searchTerm", movie);
+        fetch(movieURL)
+        .then(response => response.json())
+        .then(result => {
+            if(result.Error) {
+                setMovies([]);
+                setNoMovieFound(true);
+            } else {
+                //console.log(result);
+                setMovies(result.Search);
+                setNoMovieFound(false);
+            };
+        });
+    };
+
+    useEffect(() => {
+        let term = localStorage.getItem("searchTerm");
+
+        if (term) {
+            handleClick(term);
+        };
+    }, []);
+
+    const returnedMovies = movies.map(movie => {
+        return (
+            <div key={movie.imdbID} className="ml-2 mr-2 mb-2">
+                <NavLink to={`/${movie.imdbID}`}><img width="200px" height="300px" src={movie.Poster} alt={"Poster for " +movie.title}/></NavLink>
+            </div>
+        );
+    });
+
+    return (
+        <div className="justify-content-center">
+            <div id="header" className="col mb-4">
+                <div className="row justify-content-center mt-3">
+                    <h1>React Movie App</h1>
+                </div>
+                <div className="row justify-content-center mt-3">
+                    <label className="mr-2" htmlFor="title">Movie Title:</label>
+                    <input id="search-bar" type="text" onChange={handleChange}/>
+                </div>
+                <div className="row justify-content-center mt-3">
+                    <button className="mr-5 btn-success" onClick={() => handleClick(searchTerm)}>Search</button>
+                    <button className="btn-primary" onClick={handleClearScreen}>Clear</button>
+                </div>
+            </div>
+            
+            <div className="container d-flex flex-wrap justify-content-center">
+                {returnedMovies}
+                {noMovieFound ? <h1>No Movie Found</h1> : null}
+            </div>
+
+            <div id="footer" className="row justify-content-center pt-4 pb-3">
+                    <a className="mr-4" href="#search-bar">Search</a>
+                    <p>|</p>
+                    <a className="ml-4" target="_blank"href="http://iwt.ranken.edu/gbreeding/BlogSite/">Developer</a>
+            </div>
+        </div>
+        //  {boolean ? (if true):(if false)}
+    );
+};
+
+export default MovieList;
